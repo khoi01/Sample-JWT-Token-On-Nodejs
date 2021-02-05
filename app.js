2f0217635961a6b186e74640d1d80669e64bee0c
Abdul Qayyum <khoi00091@gmail.com>
2021-02-05 10:57:20 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const jwtPrivateKey = "secretKey";
+const jwtAlgorithm = "HS256";
+const jwtExpiredIn = "30s";
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Welcome to the API",
+  });
+});
+
+app.post("/api/posts", verifyToken, (req, res) => {
+  res.json({
+    message: "Post Created...",
+  });
+});
+
+app.post("/api/login", (req, res) => {
+  const user = {
+    id: 1,
+    username: "brad",
+    email: "brad@gmail.com",
+  };
+
+  jwt.sign({ user: user }, jwtPrivateKey,{algorithm:jwtAlgorithm,expiresIn:jwtExpiredIn}, (err, token) => {
+    //   console.log(token);
+    res.json({
+      token: token,
+    });
+  });
+});
+
+//FORMAT TOKEN
+// Authorization: Bearer <access_token>
+
+//Verify token
+function verifyToken(req, res, next) {
+  //get auth header value
+  const token = req.headers["authorization"];
+
+  //decoded :payload
+  jwt.verify(token,jwtPrivateKey, (err, decoded) => {
+      console.log(decoded);
+    if (err) {
+      res.sendStatus("403");
+    } else {
+      next();
+    }
+  });
+}
+
+app.listen(3000, () => console.log("Server started on port 3000"));
